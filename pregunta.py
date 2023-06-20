ad4a00b2ab8adfecdcd8bfc258c95efab6107c73
jossaz <51805547+jossaz@users.noreply.github.com>
2023-06-20 03:56:31 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -14,8 +14,32 @@ import pandas as pd
 
 def ingest_data():
 
-    #
-    # Inserte su código aquí
-    #
+    with open('clusters_report.txt', 'r') as file:
+    content = file.read()
+
+# Procesar los datos
+lines = content.split('\n')
+data = []
+for line in lines:
+    line = line.strip()
+    if line.startswith('Cluster') or line == '':
+        continue
+    elements = line.split()
+    cluster = {
+        'cluster': int(elements[0]),
+        'cantidad_de_palabras_clave': int(elements[1]),
+        'porcentaje_de_palabras_clave': float(elements[2].replace('%', '').replace(',', '.')),
+        'palabras_clave': ', '.join(elements[3:])
+    }
+    data.append(cluster)
+
+# Crear el DataFrame
+df = pd.DataFrame(data)
+
+# Reemplazar espacios por guiones bajos en los nombres de las columnas
+df.columns = df.columns.str.replace(' ', '_').str.lower()
+
+# Mostrar el DataFrame resultante
+print(df)
 
     return df
